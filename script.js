06d5a25d35092574e5f735dae9eaa1cd10f99800
Ilana <ilana@Justins-Macbook.lan>
2022-07-21 20:30:09 -0500

    finished

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 // TODO: add code here
+    window.addEventListener("load", function(){
+    this.fetch("https://handlers.education.launchcode.org/static/astronauts.json").then((response) =>{
+        return response.json();
+    })
+    .then((json) => {
+const container = this.document.getElementById('container');
+let astronautHTML = "";
+for (astronaut of json){
+    
+    astronautHTML += `
+    <div class="astronaut">
+          <div class="bio">
+            <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+            <ul>
+              <li>Hours in space: ${astronaut.hoursInSpace}</li>
+              <li>Active: ${astronaut.active}</li>
+              <li>Skills: ${astronaut.skills.join(', ')}</li>
+            </ul>
+          </div>
+          <img class="avatar" src=${astronaut.picture}>
+        </div>
+    `
+        }
+        container.innerHTML = astronautHTML
+    });
+});
+
